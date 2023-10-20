89b95f53565543f87e18d367fd3197b87f25bc7c
rubeniab <147776031+rubeniab@users.noreply.github.com>
2023-10-20 16:57:03 -0600

    Add files via upload

---
+++ foco.js
@@ -0,0 +1,30 @@
+window.addEventListener('load', function(){
+    foco();
+});
+
+function foco(){
+    document.querySelector('body').classList.add('oscuridad');
+
+    const titulo = document.createElement("h1");
+    titulo.classList.add("titulo");
+    const anio = new Date().getFullYear();
+    titulo.innerText = `Desarrollo de Sistemas Web ${anio}`;
+    document.body.appendChild(titulo);
+
+    const img = document.createElement("img");
+    img.classList.add('foco');
+    img.setAttribute('src', 'foco-apagado.jpg'); 
+
+    img.addEventListener('click', () => {
+        if(img.src.indexOf("apagado") > 0){
+            img.setAttribute('src', 'foco-prendido.jpg');
+            document.body.classListremove('oscuridad');
+            document.body.classList.add('luz');
+        }else{
+            img.setAttribute('src', 'foco-apagado.jpg');
+            document.body.classList.remove('luz');
+            document.body.classList.add('oscuridad');
+        }
+    });
+}
+
